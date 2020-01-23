73eef2f1305f9a59d777581256b89ede7265a6ae
dorjepadma <dorjepadma@gmail.com>
2020-01-23 10:41:36 -0800

    cart data

---
+++ tea shop/data/cart.js
@@ -0,0 +1,18 @@
+const cart = [{
+    id: 'oolong',
+    quantity: 2,
+    price: 12.00
+}, {
+    id: 'DogHeadGreen',
+    quantity: 2,
+    price: 12.00,
+}, {
+    id: 'Da Hong Pao',
+    quantity: 1,
+    price: 8.00,
+}, {
+    id: 'cooked puer',
+    quantity: 1,
+    price:4.00,
+}];
+export default cart;
